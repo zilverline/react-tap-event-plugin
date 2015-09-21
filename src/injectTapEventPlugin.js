3f6b39d168a0152758eef58c19707ac2eadb27a1
Jonathan Rouleau <jr@rlabs.io>
2015-09-21 13:53:54 -0300

    Remove React.initializeTouchEvents
    React.initializeTouchEvents is no longer necessary (since 0.12)  and has been removed
    completely. Touch events now work automatically.

--- src/injectTapEventPlugin.js
+++ src/injectTapEventPlugin.js
@@ -1,6 +1,5 @@
 module.exports = function injectTapEventPlugin () {
   var React = require("react");
-  React.initializeTouchEvents(true);
 
   require('react/lib/EventPluginHub').injection.injectEventPluginsByName({
     "ResponderEventPlugin": require('./ResponderEventPlugin.js'),
